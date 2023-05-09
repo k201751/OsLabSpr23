4dc2fc01fb0e9213794a92530c3a31eb3dedb744
k201751 <77692502+k201751@users.noreply.github.com>
2023-05-09 23:43:45 +0500

    Update mergesort.c

--- lab8/mergesort.c
+++ lab8/mergesort.c
@@ -40,13 +40,22 @@ void *merge_sort(void *arg) {
     int end = args->end;
     if (start < end) {
         int mid = (start + end) / 2;
+        
+        // Create two threads for sorting each half of the array
         pthread_t thread1, thread2;
         thread_args args1 = {start, mid};
         thread_args args2 = {mid + 1, end};
+        
+        // Sort the first half of the array using thread1
         pthread_create(&thread1, NULL, merge_sort, &args1);
+        // Sort the second half of the array using thread2
         pthread_create(&thread2, NULL, merge_sort, &args2);
+        
+        // Wait for both threads to finish before merging
         pthread_join(thread1, NULL);
         pthread_join(thread2, NULL);
+        
+        // Merge the sorted subarrays
         merge(start, mid, end);
     }
     pthread_exit(NULL);
@@ -60,10 +69,17 @@ int main() {
     for (i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
+    
+    // Create the arguments for the main sorting thread
     thread_args args = {0, n - 1};
+    
+    // Create the main sorting thread
     pthread_t main_thread;
     pthread_create(&main_thread, NULL, merge_sort, &args);
+    
+    // Wait for the main sorting thread to finish
     pthread_join(main_thread, NULL);
+    
     printf("The sorted array is:\n");
     for (i = 0; i < n; i++) {
         printf("%d ", arr[i]);
